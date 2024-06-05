61ee996ddbe7ef8f6550ac76a8b00706d5cfc138
Alexander Tibanta Miranda <119013329+Alexander-Tibanta23@users.noreply.github.com>
2024-06-04 19:19:42 -0500

    UPDATE [Proyecto 1B],  Integración Continua and jar file

--- src/main/java/dao/ExecuteDescuentoDAO.java
+++ src/main/java/dao/ExecuteDescuentoDAO.java
@@ -10,19 +10,19 @@ public class ExecuteDescuentoDAO {
 
         //Agregar Descuento
         Descuento nuevoDescuento = new Descuento();
-        nuevoDescuento.setCodigo("696969");
-        nuevoDescuento.setNombre("Descuento111");
-        nuevoDescuento.setPorcentajeDescuento(BigDecimal.valueOf(70));
-        nuevoDescuento.setStock(5);
+        nuevoDescuento.setCodigo("231231");
+        nuevoDescuento.setNombre("DescuentoBALL");
+        nuevoDescuento.setPorcentajeDescuento(BigDecimal.valueOf(40));
+        nuevoDescuento.setStock(15);
         descuentoDAO.guardarDescuento(nuevoDescuento);
 
         //Actualizar Descuento existente
-        Descuento descuentoExistente = descuentoDAO.obtenerDescuentoPorId(12);
-        descuentoExistente.setNombre("DescuentoR");
+        Descuento descuentoExistente = descuentoDAO.obtenerDescuentoPorId(4);
+        descuentoExistente.setNombre("DescuentoASDGF");
         descuentoDAO.actualizarDescuento(descuentoExistente);
 
         //Eliminar Descuento existente
-        Descuento descuentoAEliminar = descuentoDAO.obtenerDescuentoPorId(8);
+        Descuento descuentoAEliminar = descuentoDAO.obtenerDescuentoPorId(18);
         descuentoDAO.eliminarDescuento(descuentoAEliminar);
     }
 }
