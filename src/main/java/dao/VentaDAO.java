25e87e07ad13066be1cd1daf008a44054270d1bb
Alexander Tibanta Miranda <119013329+Alexander-Tibanta23@users.noreply.github.com>
2024-07-17 00:23:46 -0500

    Donoban Ramon [Sprint 2] V1.2 Implementacion funcionalidad de listado de Facturas

--- src/main/java/dao/VentaDAO.java
+++ src/main/java/dao/VentaDAO.java
@@ -2,9 +2,13 @@ package dao;
 
 import entity.DetallesVenta;
 import entity.Venta;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import util.HibernateUtil;
+
+import java.util.List;
 
 public class VentaDAO {
     public void guardarVenta(Venta venta) {
@@ -27,5 +31,33 @@ public class VentaDAO {
         } finally {
             session.close();
         }
+    }
+
+    public List<Venta> obtenerVentas() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+        List<Venta> ventas = null;
+
+        try {
+            transaction = session.beginTransaction();
+            Query<Venta> query = session.createQuery("from Venta", Venta.class);
+            ventas = query.list();
+
+            // Inicializar las colecciones perezosas
+            for (Venta venta : ventas) {
+                Hibernate.initialize(venta.getDetalles());
+            }
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        return ventas;
     }
 }
